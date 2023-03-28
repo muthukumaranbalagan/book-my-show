cf622bc7b6bd0d915ed2bbae50a6e4ecee1132fe
muthukumaranbalagan <mk4459065@gmail.com>
2023-03-28 16:11:57 +0530

    first commit

---
+++ jsc.js
@@ -0,0 +1,23 @@
+
+let name="mk";
+let age=22;
+let gender="male";
+
+let person={
+    name:"mk",
+     age:22,
+ gender:"male",
+ sibiling:{
+    bro:"ram"
+}
+}
+console.log(person.sibiling.bro); 
+
+let names=["sarvan","mk","vj"];
+console.log(names[2]);
+names[4]=["vimal"];
+names[5]=[5];
+console.log(names);
+let a=3;
+let b=5;
+console.log(b>a);
