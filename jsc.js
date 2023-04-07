3672f30beeadc6da720a706c8f477409c5bdb376
muthukumaranbalagan <mk4459065@gmail.com>
2023-04-07 13:56:47 +0530

    first commit

--- jsc.js
+++ jsc.js
@@ -1,23 +1,74 @@
+// const person={
+//     name:"mk",
+//     age:22,
+//     gender:"m" ,
+//       sibiling:{
+//         brother:"jjey",
 
-let name="mk";
-let age=22;
-let gender="male";
+//      }
+// }
+// for(let key in person){
+//     console.log(person.name);
+// }
 
-let person={
-    name:"mk",
-     age:22,
- gender:"male",
- sibiling:{
-    bro:"ram"
-}
-}
-console.log(person.sibiling.bro); 
+//  let person=
+//  {
+//     name:"mk",
+//     age:23,
+//     course:["html",'css','javascript'],
+//     location:
+//     {
+//         state:'tamilnadu',
+//         town:'usilampatti'
+//     },
+//     addition()
+//    {
+//         let show=this.name+this.age+this.course+this.location;
+//         console.log(show);
+//     }
+// };
+// // person.addition();
 
-let names=["sarvan","mk","vj"];
-console.log(names[2]);
-names[4]=["vimal"];
-names[5]=[5];
-console.log(names);
-let a=3;
-let b=5;
-console.log(b>a);
+// function parent( name){
+//     return{
+//         name:name,
+//          greet( ){
+//            let msg="this is "+this.name;
+//            console.log(msg);
+//         }
+//    }
+// }
+//    parent("mk").greet();
+   
+// }
+// let a=parent("mk");
+// a.greet();
+
+// function Person(nam){
+//     this.name=nam;
+//     this.greeting=function(){
+//         let msg="this is "+this.name;
+//         console.log(msg);
+//         }
+// }
+// let p=new Person("ass");
+// p.greeting();
+// let c={
+//     value:5};
+// function cart(b){
+//   b.value++;
+// }
+// cart(c);
+// console.log(c);
+// let ae={
+//     name:"mk",
+//     greet(){
+//         console.log(`my name is ${this.name}`);
+//     }
+// }
+// for(let key of Object.entries(ae)){
+//     console.log(key);
+// }
+       
+let str="muthu";
+console.log(str.charAt(2));
